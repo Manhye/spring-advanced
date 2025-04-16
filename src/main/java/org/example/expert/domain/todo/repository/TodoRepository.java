bd5283f17894100321949cd5df6289ea494adcb7
kibeom0806@gmail.com <kibeom0806@gmail.com>
2025-04-16 17:30:51 +0900

    Level 2 : N + 1 Problem

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +12,13 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+//    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+//    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+//    // LEFT JOIN FETCH t.user u 에서 Todo 엔티티를 조회할 때 연관된 user 엔티티를 즉시 로딩한다.
+
+    @EntityGraph(attributePaths = "user")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN FETCH t.user " +
